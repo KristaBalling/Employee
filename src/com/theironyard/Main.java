0e8aec239567288a71da7b19ee058bbcfb86e213
Krista Blachian <krista.blachian@scifusion360.com>
2019-01-29 09:13:30 -0500

    testing with lamdas

--- src/com/theironyard/Main.java
+++ src/com/theironyard/Main.java
@@ -2,6 +2,7 @@ package com.theironyard;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.IntPredicate;
 import java.util.function.Predicate;
 
 public class Main {
@@ -26,6 +27,24 @@ public class Main {
 
         printEmployeesByAge(employees, "Employees over 30", employee -> employee.getAge()> 30);
         printEmployeesByAge(employees, "\nEmployees 30 and under", employee -> employee.getAge()<= 30);
+        printEmployeesByAge(employees, "\nEmployees younger than 25", new Predicate<Employee>() {
+            @Override
+            public boolean test(Employee employee) {
+                return employee.getAge() < 25;
+            }
+        });
+
+        //testing with lambdas
+        IntPredicate greaterThan15 = i -> i > 15;
+        IntPredicate lessThan100 = i -> i < 100;
+        System.out.println(greaterThan15.test(10));
+        int a = 20;
+        System.out.println(greaterThan15.test(a + 5));
+
+        System.out.println(greaterThan15.and(lessThan100).test(50));
+        System.out.println(greaterThan15.and(lessThan100).test(15));
+
+
 
 
     }
